e1919c99dbc3a8b5fe9761a2c4583535965e7fdc
suhahmed <suhail.asyed@gmail.com>
2017-01-09 19:04:13 +0530

    commit

--- chef-repo/cookbooks/yum_repository/recipes/default.rb
+++ chef-repo/cookbooks/yum_repository/recipes/default.rb
@@ -6,3 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+yum_repository "microsoft-dsc" do
+	description "Microsoft Repo"
+	baseurl "http://10.121.48.123/microsoft/"
+	enabled "True"
+	gpgcheck "False"
+	action :create
+end
+
+yum_package "omi-1.1.0.ssl_100.x64.rpm" do
+	action :install
+	flush_cache [:before]
+	end
+
+yum_package "dsc-1.1.1-294.ssl_100.x64.rpm" do
+	action :install
+	flush_cache [:before]
+	end
